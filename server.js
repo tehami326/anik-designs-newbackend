381507ac4db20f4d83339f87eb74eb958468d4e4
Muhammad Tehami <151735000+tehami326@users.noreply.github.com>
2026-04-22 18:52:55 +0530

    Switch to CommonJS require syntax in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config();
 
-import app from "./src/app.js";
-import mongoose from "mongoose";
+const app = require("./src/app");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
 
