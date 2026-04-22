a404da3d519363643000644e256708457c3ca376
Muhammad Tehami <151735000+tehami326@users.noreply.github.com>
2026-04-22 19:24:58 +0530

    Add user management routes for admin

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,6 +4,16 @@ const router = express.Router();
 const { getAdminStats } = require("../controllers/adminController");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
+const {
+    getAllUsers,
+    deleteUser,
+    updateUserRole,
+} = require("../controllers/userController");
+
 router.get("/stats", protect, adminOnly, getAdminStats);
+
+router.get("/users", protect, adminOnly, getAllUsers);
+router.delete("/users/:id", protect, adminOnly, deleteUser);
+router.put("/users/:id/role", protect, adminOnly, updateUserRole);
 
 module.exports = router;
