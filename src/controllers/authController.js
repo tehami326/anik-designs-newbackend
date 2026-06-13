9d23b56f48ddf67fcbd2a56d9a98ccf9058e9203
Muhammad Tehami <151735000+tehami326@users.noreply.github.com>
2026-06-13 11:27:42 +0530

    Extend JWT token expiration to 90 days

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
+        expiresIn: "90d",
     });
 };
 
